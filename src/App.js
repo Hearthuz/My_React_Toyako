5ee76023d5c2f5951be640b2e66f9ab7ba65d94d
John Doe <johndoe@example.com>
2022-10-17 10:46:31 +0700

    test101

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React AI HERE NEW
         </a>
       </header>
     </div>
